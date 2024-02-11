c4b27fda82e6e004c68f507c2bfe2286ec62ce87
emmebroman <bromanem@arcada.fi>
2024-02-11 17:28:16 +0200

    Rest client funkar, get + post

--- api/cart.js
+++ api/cart.js
@@ -1,9 +1,37 @@
-function handler(req, res) {
-    res.status(200).json({
-      method: req.method,
-      hello: "Cart Service"
-    });
-  }
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+const app = express();
 
 
-  module.exports = handler;
+
+const db_URL = process.env.DATABASE_URL;
+
+
+const pool = new Pool({
+    connectionString: db_URL,
+  });
+
+
+app.get('/', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM products');
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error executing query', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+app.post('/', async (req, res) => { 
+    const { customerid, product, amount, price } = req.body;
+
+    try {
+        const result = await pool.query('INSERT INTO products (customerid, product, amount, price) VALUES ($1, $2, $3, $4) RETURNING *', [customerid, product, amount,price]);
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error adding product to the database:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+module.exports = app;
--- server.js
+++ server.js
@@ -3,41 +3,25 @@ const handler = require('./api/cart.js');
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const app = express();
+
+const PORT = 3000; 
 
 const db_URL = process.env.DATABASE_URL;
-const app = express();
-const PORT = 3000; // You can use any port number you prefer
+
 
 const pool = new Pool({
     connectionString: db_URL,
   });
 
 
-  //app.use(express.json());
+  app.use(express.json());
 
-// Route for handling requests
-//app.use('/', handler);
+  app.use('/', express.static(__dirname + '/'))
 
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-      console.error('Error executing query', err);
-    } else {
-      console.log('Connected to database. Current time:', res.rows[0].now);
-    }
-   
-  });
+  const cart = require('./api/cart.js')
+  app.use('/cart', cart)
 
-  
-app.get('/', async (req, res) => {
-    try {
-      const result = await pool.query('SELECT * FROM products');
-      res.json(result.rows);
-    } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-  
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
