aaee073b544e4c5a27ee808bd1e470d73ec413b6
emmebroman <bromanem@arcada.fi>
2024-02-29 07:12:45 +0200

    Koden kommenterad

--- api/cart.js
+++ api/cart.js
@@ -4,83 +4,112 @@ require('dotenv').config();
 const app = express();
 const db_URL = process.env.DATABASE_URL;
 
+
+//Connectar till databasen
 const pool = new Pool({
     connectionString: db_URL,
-  });
+});
 
 
+//Hanterar GET request till endpointen
 app.get('/', async (req, res) => {
     try {
-      const result = await pool.query('SELECT id, user_id, products, quantity, price FROM products');
-      res.json(result.rows);
-      
+        //Executar en SQL SELECT query på tabellen "products" (tabellen innehåller alla carts) genom att använda pool
+        //Get alla kolumner i tabellen, väljer ordning hur dom visas genom att skriva ut kolumnnamnen
+        const result = await pool.query('SELECT id, user_id, products, quantity, price FROM products');
+
+        //Skickar JSON response med result
+        res.json(result.rows);
+
+        //Felhantering
     } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
-  
-  app.get('/:id', async (req, res) => {
+});
+
+//Hanterar GET request för specifik cart_id
+app.get('/:id', async (req, res) => {
+    //Hämtar cart_id från url
     const cartId = req.params.id;
     try {
-      const result = await pool.query('SELECT id, user_id, products, quantity, price FROM products WHERE id = $1', [cartId]);
-      if (result.rows.length > 0) {
-        res.json(result.rows);
-    } else {
-        res.status(404).json({error: 'Cart not found'})
-    }
-    } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+        //Executar eb SQL SELECT query på tabellen för det specifika cart_id
+        const result = await pool.query('SELECT id, user_id, products, quantity, price FROM products WHERE id = $1', [cartId]);
 
-app.post('/', async (req, res) => { 
+        //KOllar om det finns ett resultat, isåfall skrivs result ut, annars skrivs ett felmeddelande ut
+        if (result.rows.length > 0) {
+            res.json(result.rows);
+        } else {
+            res.status(404).json({ error: 'Cart not found' })
+        }
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//Executar en POST request till endpointen
+app.post('/', async (req, res) => {
+    //Hämtar data från request bodyn
     const { user_id, product, quantity, price } = req.body;
 
     try {
-        const result = await pool.query('INSERT INTO products (user_id, product, quantity, price) VALUES ($1, $2, $3, $4) RETURNING *', [user_id, product, quantity,price]);
+        //Executar en SQL INSERT query för att lägga till en ny rad i tabellen "producs"t"
+        const result = await pool.query('INSERT INTO products (user_id, product, quantity, price) VALUES ($1, $2, $3, $4) RETURNING *', [user_id, product, quantity, price]);
+        //Om insättningen lyckas skrivs resultatet ut
         res.status(201).json(result.rows[0]);
+
+        //Felhantering
     } catch (error) {
         console.error('Error adding cart to the database:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
+// Hanterar PATCH requests för en specifik cart_id till endpointen
 app.patch('/:id', async (req, res) => {
+    //Hämtar id från request URL
     const cartId = req.params.id;
-    const {user_id, product, quantity, price}=req.body;
+    //Hämtar data från request bodyn
+    const { user_id, product, quantity, price } = req.body;
 
-    try{
+    try {
+        //Executar en SQL  UPDATE query för att uppdatera raden för den specifika id:n i tabellen "products"
         const result = await pool.query(
             'UPDATE products SET user_id = $1, product = $2, quantity = $3, price = $4 WHERE id = $5 RETURNING *',
             [user_id, product, quantity, price, cartId]
         );
-
+        //Om ingen rad hittades
         if (result.rows.length === 0) {
             return res.status(404).json({ error: 'Cart not found' });
         }
-
+        //Skriv ut den nya raden  ifall raden hittades och uppdaterades
         res.json(result.rows[0]);
+
+        //felhantering
     } catch (error) {
         console.error('Error updating cart in the database:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-    }
+}
 )
-
+//Hanterar DELETE request för en specifik cart_id
 app.delete('/:id', async (req, res) => {
+    //Hämtar cart_id från request url
     const cartId = req.params.id;
 
     try {
+        //Executar en SQL DELETE query för den specifika cart_id i tabellen "products"
         const result = await pool.query('DELETE FROM products WHERE id = $1', [cartId]);
 
+        //Ifall ingen rad hittas
         if (result.rowCount === 0) {
 
             return res.status(404).json({ error: 'Cart not found' });
         }
-
+        //Om borttagningen lyckades
         res.json({ message: 'Cart deleted successfully' });
+
+        //Felhantering
     } catch (error) {
         console.error('Error deleting cart from the database:', error);
         res.status(500).json({ error: 'Internal server error' });
